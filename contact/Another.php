3b7b086f73d175197aa4bdac4fb2b76aea9d8696
chickenjieb <chickenjieb@gmail.com>
2017-04-20 01:31:31 +0700

    Google contacts

---
+++ contact/Another.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <!--Import Google Icon Font-->
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+    <link type="text/css" rel="stylesheet" href="css/editcss.css" />
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <!-- Nav bar -->
+   <?php include('navbar.php'); ?>
+  </head>
+
+  <body>
+ <div class="container often">
+    <ul class="collection often">
+      <li class='collection-item often' style="color: #616161;">รายชื่อติดต่ออื่นๆ</li>
+      <?php 
+          for($i=0; $i<15; $i++){
+          echo "<li class='collection-item often avatar hoverable' onclick='$('#modal-Often').modal('open');''>
+            <div class='row'>
+              <div class='col s1'>
+                <i class='material-icons circle purple'>J</i>
+              </div>
+              <div class='col s4'>
+                <p>junior8807@yahoo.co.th</p>
+              </div>
+              <div class='col s4 hide-on-small-only hide-on-med-only'>
+                <p>junior8807@yahoo.co.th</p>
+              </div>
+              <div class='col s3 right-align grey-text hide-on-small-only hide-on-med-only'>
+                <i class='material-icons'>star_border</i>
+                <i class='material-icons'>person_add</i>
+                <i class='material-icons'>more_vert</i>
+              </div>
+            </div>
+          </li>";
+        } 
+      ?>
+    </ul>
+  </div>
+
+   <!-- <a class="waves-effect waves-light btn view" onclick="$('#modal-Family').modal('open');">Add Tag</a> -->
+  <!-- Modal Family -->
+  <div id="modal-Often" class="modal">
+    <div class="modal-footer">
+      <a href="#" class=" modal-action modal-close waves-effect waves-green btn-flat">ไปยัเวอร์ชันเก่า</a>
+      <a href="#" class=" modal-action modal-close waves-effect waves-green btn-flat">ยกเลิก</a>
+    </div>
+    <div class="modal-content">
+      <h4><i class="material-icons circle">account_circle</i>ป่าป๊า</h4>
+      <p>รายละเอียดการติดต่อ</p><br>
+      <p><i class="material-icons">email</i><a href="#">&nbsp;junior8807@yahoo.co.th</a></p>
+    </div>
+    
+  </div>
+
+    <!--Import jQuery before materialize.js-->
+    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script type="text/javascript" src="js/materialize.min.js"></script>
+    <script type="text/javascript" src="js/editjs.js"></script>
+
+
+  </body>
+</html>
+        
---
+++ contact/Duplicate.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <!--Import Google Icon Font-->
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+    <link type="text/css" rel="stylesheet" href="css/editcss.css" />
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <!-- Nav bar -->
+   <?php include('navbar.php'); ?>
+  </head>
+
+  <body class="white">
+  
+  <div class="container" style="width:90%;">
+    <p style="display:block; float:left; color:grey;">1 รายการซ้ำ<p>
+    <p style="color: #4274FF; display:block; float:right;">รวมทั้งหมด<p>
+      <div class="row" style="margin-top:30px;">
+        <div class="col s12">
+          <div class="card dup" style="margin-top:30px;">
+            <div class="card-content dup">
+              <ul class="collection">
+                <li class="collection-item dup"><h5>Jieb TruemoveH</h5></li>
+                <li class="collection-item dup avatar">
+                  <i class="material-icons circle">account_circle</i>
+                  <span class="title">Jieb TruemoveH</span>
+                  <p>0954860000(Mobile)</p>
+                </li>
+                <li class="collection-item dup avatar">
+                  <i class="material-icons circle">account_circle</i>
+                  <span class="title">Jieb TruemoveH</span>
+                  <p>0954860000(Mobile)</p>
+                </li>
+              </ul>
+            </div>
+            <div class="card-action right-align">
+              <a class="blue-text" href="#">รวม</a>
+              <a class="blue-text" href="#">ปิด</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!--Import jQuery before materialize.js-->
+    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script type="text/javascript" src="js/materialize.min.js"></script>
+    <script type="text/javascript" src="js/editjs.js"></script>
+
+
+  </body>
+</html>
+        
